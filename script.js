0f33310e6994ae29a8db868c6a6c2cc846462cf6
debbieeelee <75164598+debbieeelee@users.noreply.github.com>
2026-06-03 14:13:08 +0900

    Update script.js

--- script.js
+++ script.js
@@ -583,7 +583,8 @@
           }
         });
       },
-      { threshold: 0.1, rootMargin: '0px 0px -40px 0px' }
+      { threshold: 0.01, rootMargin: '0px 0px 100px 0px' }
+      // script.js 수정 전
     );
 
     $$('.fade-in').forEach(el => scrollObserver.observe(el));
